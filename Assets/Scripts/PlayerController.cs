d954243c69cf7e9d86d41611bab81498937af523
nicholasarinduque <89489431+nicholasarinduque@users.noreply.github.com>
2022-04-19 15:38:32 -0700

    2.14 done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10.0f; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal")    
+        if (transform.postion = new Vector3(-10, transform.postion.y, transform.postion.z);
+        {
+
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
